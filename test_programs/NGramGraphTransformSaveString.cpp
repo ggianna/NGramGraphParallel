d421dfff11e45c9300fa55e6020bbd6c36a18b2b
ngialitsis <ngialitsis@scify.gr>
2021-06-09 15:46:08 +0300

    first compile

--- test_programs/NGramGraphTransformSaveString.cpp
+++ test_programs/NGramGraphTransformSaveString.cpp
@@ -33,6 +33,9 @@ int main(int argc, char* argv[]){
 	InputParser parser(argc,argv);
 	
 	std::string TEXT_PAYLOAD =  parser.cmdOptionExists("-s") ? parser.getCmdOption("-s") : "";
+	std::string S1 =  parser.cmdOptionExists("-s1") ? parser.getCmdOption("-s1") : "";
+	std::string S2 =  parser.cmdOptionExists("-s2") ? parser.getCmdOption("-s2") : "";
+	
 	int WINDOWSIZE =  parser.cmdOptionExists("-w") ?  atoi(parser.getCmdOption("-w").c_str()) : NGRAMSIZE_DEFAULT_VALUE;	
 	int NGRAMSIZE_VALUE =  parser.cmdOptionExists("-n") ? atoi(parser.getCmdOption("-n").c_str()) : 2;
 
@@ -42,12 +45,29 @@ int main(int argc, char* argv[]){
 
 	StringSplitter testStringSplitter(NGRAMSIZE_VALUE);
 
-	StringPayload testStringPayload(TEXT_PAYLOAD);
-
+	
 	ProximityApproach* approach = new SymmetricApproach();
+	
+	StringPayload testStringPayload(S1);
+	NGramGraph NGG1(nullptr, &testStringSplitter, &testStringPayload, WINDOWSIZE, approach);
+	NGG1.createGraph();
+	NGG1.printGraphviz();
+	
+	StringPayload testStringPayload2(S2);
+	NGramGraph NGG2(nullptr, &testStringSplitter, &testStringPayload2, WINDOWSIZE, approach);
+	NGG2.createGraph();
+	NGG2.printGraphviz();
+	
+	GraphSimilarity testGraphSimilarity;
+	GraphComparator<std::string, std::string> testGraphComparator;
+	testGraphSimilarity = testGraphComparator.compare(NGG1, NGG2);
 
-	NGramGraph testNGramGraph(nullptr, &testStringSplitter, &testStringPayload, 2, approach);
-	testNGramGraph.createGraph();
-	testNGramGraph.printGraphviz();
-	testNGramGraph.printGraphvizToFile("out.dot");
-}
+
+	map<string, double> testSimilarityComponents =  testGraphSimilarity.getSimilarityComponents();
+    	cout << "Size Similarity: " << testSimilarityComponents["sizeSimilarity"] << endl;
+    	cout << "Value Similarity: " << testSimilarityComponents["valueSimilarity"] << endl;
+    	cout << "Normalized Value Similarity: " << testSimilarityComponents["normalizedValueSimilarity"] << endl;
+    	cout << "Containment Value Similarity: " << testSimilarityComponents["containmentSimilarity"] << endl;
+
+
+	}
